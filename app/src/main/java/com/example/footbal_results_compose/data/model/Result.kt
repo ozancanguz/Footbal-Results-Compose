fd79f0b06272d406ef8d569966595e4954fbff08
ozancanguz <ozancanguz24@gmail.com>
2023-10-16 15:11:05 +0300

    data classes created

---
+++ app/src/main/java/com/example/footbal_results_compose/data/model/Result.kt
@@ -0,0 +1,8 @@
+package com.example.footbal_results_compose.data.model
+
+data class Result(
+    val away: String,
+    val date: String,
+    val home: String,
+    val skor: String
+)
---
+++ app/src/main/java/com/example/footbal_results_compose/data/model/Scores.kt
@@ -0,0 +1,6 @@
+package com.example.footbal_results_compose.data.model
+
+data class Scores(
+    val result: List<Result>,
+    val success: Boolean
+)
