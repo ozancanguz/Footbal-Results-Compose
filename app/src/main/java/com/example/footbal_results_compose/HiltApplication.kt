6ecf265384014e6fedce962e4013a2d04984c5e1
ozancanguz <ozancanguz24@gmail.com>
2023-10-16 15:13:03 +0300

    hilt added

---
+++ app/src/main/java/com/example/footbal_results_compose/HiltApplication.kt
@@ -0,0 +1,10 @@
+package com.example.footbal_results_compose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication: Application() {
+
+
+}
